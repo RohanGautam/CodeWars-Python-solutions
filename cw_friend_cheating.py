d1625882c4a6f5024157c768df5a30ce3b1a5ab8
Rohan Gautam <rohan1gautam@gmail.com>
2016-12-21 19:24:37 +0530

    Add files via upload

---
+++ cw_friend_cheating.py
@@ -0,0 +1,27 @@
+##A friend of mine takes a sequence of numbers from 1 to n (where n > 0).
+##Within that sequence, he chooses two numbers, a and b.
+##He says that the product of a and b should be equal to the sum
+##of all numbers in the sequence, excluding a and b.
+##Given a number n, could you tell me the numbers he excluded
+##from the sequence?
+##The function takes the parameter:
+##n (don't worry, n is always strictly greater than 0 and small
+##enough so we shouldn't have overflow) and returns an array of the form:
+##
+##[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+##with all (a, b)which are the possible removed numbers in the
+##sequence 1 to n.
+
+
+def removNb(n):
+    L=range(1,n+1)
+    L2=[]
+    for a in range(len(L)):
+        for b in range(len(L)):            
+            sumL=sum([x for x in L if x!=L[a] and x!=L[b]])            
+            if L[a]*L[b]==sumL:
+                L2.append(tuple([L[a],L[b]]))
+
+    return L2
+
+print removNb(26)
